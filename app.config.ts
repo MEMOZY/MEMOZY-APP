03c04ed7f710dd0fade511a414e39426f6cb3f01
seokkkkkk <tjr6613@seoultech.ac.kr>
2025-11-26 23:45:37 +0900

    chore: 버전과 빌드번호를 각각 1.0.2, 2025112601로 업데이트

--- app.config.ts
+++ app.config.ts
@@ -3,7 +3,7 @@ import { ExpoConfig, ConfigContext } from "expo/config";
 export default ({ config }: ConfigContext): ExpoConfig => ({
     name: "memozy",
     slug: "memozy",
-    version: "1.0.1",
+    version: "1.0.2",
     orientation: "portrait",
     icon: "./assets/images/icon.png",
     scheme: "memozy",
@@ -14,7 +14,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
         supportsTablet: true,
         bundleIdentifier: "com.tym.memozy",
         usesAppleSignIn: true,
-        buildNumber: "2025100101",
+        buildNumber: "2025112601",
         infoPlist: {
             ITSAppUsesNonExemptEncryption: false,
             NSAppTransportSecurity: {
